5ea1c1136b8727bcdf23da69bf80777fcaf2099f
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-08-08 16:18:12 +0430

    Changed Page Search Style

--- src/components/TVSearch/index.js
+++ src/components/TVSearch/index.js
@@ -2,6 +2,15 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      secondary: {
+        main: "#fafafa",
+      },
+    },
+  });
 
 
 
@@ -17,8 +26,16 @@ function Filter(props){
                 options={props.list.map((item)=>{
                     return{label:item.name,id:item.id}
                 })}
-                sx={{ width: 300}}
-                renderInput={(params) => <TextField {...params} label="Type Your Selection" variant='standard' />}
+                sx={{ width: 400}}
+                renderInput={(params) => <ThemeProvider theme={theme}>
+                    <TextField {...params} 
+                    // fullWidth
+                    sx={{"& .MuiInputLabel-root": { color: "white" }, borderBottom: "1px solid white", "& .MuiInput-input":{color: "white"}}}
+                    type="search"
+                    color="secondary"
+                    label="Search ..." 
+                    variant='standard'/>
+                    </ThemeProvider>}
                 onChange={(e,data)=>{
                     setVal(data);
                     navigate(`/tv/${data.id}`);
