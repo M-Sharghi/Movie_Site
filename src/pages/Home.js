097bc7df29484366a4b0027727ca199eeca16260
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-08-10 22:23:22 +0430

    Changed Home Style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,47 @@
-import Nav from "../components/Nav";
+import Nav from "../components/NavHome";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
-import {get_multi_search} from "../helpers/server";
+import { get_multi_search } from "../helpers/server";
 import MultiSearch from "../components/MultiSearch";
 
-
-function Home(){
-
-  let [myOptions, setMyOptions] = useState([])
+function Home() {
+  let [myOptions, setMyOptions] = useState([]);
   console.log(myOptions);
-  
-    useEffect(() => {
-      get_multi_search(myOptions).then((response) => {
+
+  useEffect(() => {
+    get_multi_search(myOptions).then((response) => {
       for (var i = 0; i < response.results.length; i++) {
-        myOptions.push(response.results[i].title)
+        myOptions.push(response.results[i].title);
       }
-      setMyOptions(myOptions)
+      setMyOptions(myOptions);
     });
   }, []);
 
-  
-    return (
-      <div>
-        <Nav />
-        <img className="home" />
-        <h1 className="auto welcome">Welcome. Millions of movies, TV shows and people to discover.</h1>
-        <input type="search" placeholder="Search for ..." style={{width:"500px",  borderRadius:"48px", padding:"20px",  border:"none", fontSize:"20px"}}></input>
-        <div><img src="./icons/movieLogo.jpg"  style={{width:"200px", padding:"24px"}}  /></div>
-        <MultiSearch data={myOptions} func={get_multi_search}/><br /><br />
-        <Footer />
+  return (
+    <div>
+      <Nav />
+      <div className="home">
+        <h1 className="auto welcome">
+          Welcome. Millions of movies, TV shows and people to discover.
+        </h1>
+        <input
+          type="search"
+          placeholder="Search for ..."
+          style={{
+            width: "600px",
+            borderRadius: "48px",
+            padding: "24px",
+            border: "none",
+            fontSize: "20px",
+          }}
+        ></input>
+        <MultiSearch data={myOptions} func={get_multi_search} />
+        <br />
+        <br />
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
+}
 
-  
-  export default Home;
+export default Home;
