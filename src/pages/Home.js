826cb5212d6be89836d473b1813917d35e4fd499
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-08-11 15:06:52 +0430

    Add img to search value

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,14 @@ import { Link } from "react-router-dom";
 function Home() {
   let [data, setData] = useState([]);
   let [input, setInput] = useState("!");
+  
 
   useEffect(() => {
     get_multi_search(input).then((response) => {
       setData(response.results);
     });
   }, [input]);
+
 
   return (
     <div>
@@ -33,18 +35,26 @@ function Home() {
             border: "none",
             fontSize: "20px",
           }}
-        ></input><br /><br />
+        ></input><br />
+        <div className="auto" style={{width:'600px', textAlign:"left", padding:"16px 32px", borderRadius:16}}>
         {data.map((item)=>{
-          if(item.media_type==="person")
-            {return(<div><Link to={`/people/${item.id}`} key={item.id}>{item.name}</Link></div>);
+          let img_url="https://www.themoviedb.org/t/p/w94_and_h141_bestv2";
+          let img_people=`${img_url}${item.profile_path || item.poster_path}`;
+          let woman_img="/icons/woman.jpg";
+          let man_img="/icons/man.jpg";
+          let gender_img=`${item.gender}`==1 ? `${woman_img}` : `${man_img}` ; 
+          let img_is_null=`${item.profile_path || item.poster_path}`!== "null" ? `${img_people}` : `${gender_img}` ;
+          if(item.media_type==="person"){
+            return(<div><Link  to={`/people/${item.id}`} key={item.id}><img src={img_is_null} width="48px" height="48px" style={{margin:"0.5px 16px", borderRadius:4}}/>{item.name}</Link></div>);
             } 
           else if(item.media_type==="movie"){
-            return(<div><Link to={`/movie/${item.id}`} key={item.id}>{item.title}</Link></div>);
+            return(<div><Link  to={`/movie/${item.id}`} key={item.id}><img src={img_is_null} width="48px" height="48px" style={{margin:"0.5px 16px", borderRadius:4}}/>{item.title}</Link></div>);
             }
           else if(item.media_type==="tv"){
-            return(<div><Link to={`/tv/${item.id}`} key={item.id}>{item.name}</Link></div>);
+            return(<div><Link  to={`/tv/${item.id}`} key={item.id}><img src={img_is_null} width="48px" height="48px" style={{margin:"0.5px 16px", borderRadius:4}}/>{item.name}</Link></div>);
           }
         })}
+        </div>
       </div>
       <Footer />
     </div>
