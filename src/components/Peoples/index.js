f99e5706bf774f6a133ac734fc6de2309cf1e4ea
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-08-22 19:59:31 +0430

    Response people page & people search

--- src/components/Peoples/index.js
+++ src/components/Peoples/index.js
@@ -1,4 +1,4 @@
-import { People_name, Pic_flex, Image} from "./styles";
+import { People_name, Pic_flex, Image, Mob_Pic_flex, Mob_Image, Mob_People_name} from "./styles";
 import {Link} from "react-router-dom";
 
 
@@ -13,13 +13,21 @@ function Peoples(props){
                 let man_img="/icons/man.jpg";
                 let gender_img=`${item.gender}`==1 ? `${woman_img}` : `${man_img}` ; /* check img gender*/
                 let img_is_null=`${item.profile_path}`!== "null" ? `${img_people}` : `${gender_img}` ; /* check if img is null*/
-              return (
+                if (window.innerWidth > 480) {
+                return (
                     <Pic_flex key={item.id}>
                         <Link to={`/people/${item.id}`}><Image src={img_is_null} ></Image></Link>
                         <Link to={`/people/${item.id}`}><People_name>{item.name}</People_name></Link>
                     </Pic_flex>
-                );
-            })}
+                )}else if (window.innerWidth <= 480) {
+                    return (
+                        <Mob_Pic_flex key={item.id}>
+                            <Link to={`/people/${item.id}`}><Mob_Image src={img_is_null} ></Mob_Image></Link>
+                            <Link to={`/people/${item.id}`}><Mob_People_name>{item.name}</Mob_People_name></Link>
+                        </Mob_Pic_flex>
+                        )
+                    }
+                })}
             </div>
         </div>
     );
--- src/components/Peoples/styles.js
+++ src/components/Peoples/styles.js
@@ -9,7 +9,6 @@ let People_name = styled.div`
   height: 32px;
 `;
 
-
 let Pic_flex = styled.div`
     display: flex;
     flex-direction:column;
@@ -23,4 +22,26 @@ let Image = styled.img`
 `;
 
 
-export { People_name, Pic_flex, Image };
+let Mob_People_name = styled.div`
+  width: 120px;
+  padding:10px 8px 10px 12px;
+  // padding-left: 16px;
+  font-size: 10px;
+  text-align: left;
+  height: 24px;
+`;
+
+let Mob_Pic_flex = styled.div`
+    display: flex;
+    flex-direction:column;
+    padding-top:32px
+`;
+
+let Mob_Image = styled.img`
+  width: 100px;
+  height: 180px;
+  border-radius: 12px;
+`;
+
+
+export { People_name, Pic_flex, Image, Mob_People_name, Mob_Pic_flex, Mob_Image };
